2c36986761a6b37adc40cebefe90d00dcaa3b51f
Heneros <punk60826@gmail.com>
2021-06-17 02:56:44 +0300

    reviews section

--- js/main.js
+++ js/main.js
@@ -5,7 +5,12 @@ $(function(){
       dots: true,
       autoplay: true,
       fade: true
-
-  })
+  });
+  $('.reviews__slider').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  });
 
 });
